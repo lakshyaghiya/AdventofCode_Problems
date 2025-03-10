aeea735290b63925de9fbdcdc674dec73e1c2220
Lakshya Ghiya <lakshyaghiya@microsoft.com>
2025-03-10 12:00:17 +0530

    Day8 Part 1

---
+++ Day8 Part 1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] values = File.ReadAllLines("input.txt");
+        int rows = values.Length;
+        int cols = values[0].Length;
+        Dictionary<char, List<(int, int)>> antennas = new();
+        HashSet<(int, int)> antinodes = new();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                char c = values[i][j];
+                if (char.IsLetterOrDigit(c))
+                {
+                    if (!antennas.ContainsKey(c))
+                        antennas[c] = new List<(int, int)>();
+                    antennas[c].Add((i, j));
+                }
+            }
+        }
+        foreach (var antenna in antennas)
+        {
+            var positions = antenna.Value;
+            int count = positions.Count;
+            for (int a = 0; a < count; a++)
+            {
+                for (int b = a + 1; b < count; b++)
+                {
+                    int x1 = positions[a].Item1;
+                    int y1 = positions[a].Item2;
+                    int x2 = positions[b].Item1;
+                    int y2 = positions[b].Item2;
+
+                    int dx = x2 - x1;
+                    int dy = y2 - y1;
+
+                    int x3 = x1 - dx;
+                    int y3 = y1 - dy;
+                    int x4 = x2 + dx;
+                    int y4 = y2 + dy;
+
+                    if (IsValid(x3, y3, rows, cols)) antinodes.Add((x3, y3));
+                    if (IsValid(x4, y4, rows, cols)) antinodes.Add((x4, y4));
+                }
+            }
+        }
+
+        Console.WriteLine(antinodes.Count);
+    }
+
+    static bool IsValid(int x, int y, int rows, int cols)
+    {
+        return x >= 0 && x < rows && y >= 0 && y < cols;
+    }
+}
