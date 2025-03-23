18fcad76300f4ae4ca490dbe8fbc32953be4026c
Lakshya Ghiya <lakshyaghiya@microsoft.com>
2025-03-24 02:06:40 +0530

    Day19

---
+++ Day19 Part1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("input_day19.txt");
+        HashSet<string> towelPatterns = new HashSet<string>(lines[0].Split(", "));
+        List<string> designs = new List<string>();
+        for (int i = 2; i < lines.Length; i++)
+        {
+            if (!string.IsNullOrWhiteSpace(lines[i]))
+            {
+                designs.Add(lines[i]);
+            }
+        }
+        int validDesigns = 0;
+        foreach (string design in designs)
+        {
+            if (BFS(design,towelPatterns))
+            {
+                validDesigns++;
+            }
+        }
+        Console.WriteLine(validDesigns);
+    }
+
+    static bool BFS(string design,HashSet<string> towels)
+    {
+        Queue<string> queue = new Queue<string>();
+        HashSet<string> visited = new HashSet<string>();
+
+        queue.Enqueue(design);
+        visited.Add(design);
+
+        while (queue.Count>0)
+        {
+            string current = queue.Dequeue();
+            if (current=="")
+            {
+                return true;
+            }
+            foreach (string towel in towels)
+            {
+                if (current.StartsWith(towel))
+                {
+                    string next=current.Substring(towel.Length);
+                    if (!visited.Contains(next))
+                    {
+                        queue.Enqueue(next);
+                        visited.Add(next);
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Day4 Part1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        var lines = File.ReadAllLines("input.txt");
+        int ans = CountXMAS(lines, "XMAS");
+        Console.WriteLine(ans);
+
+    }
+    static int CountXMAS(string[] lines, string word)
+    {
+        int count = 0;
+        int length = lines.Length;
+        int width = lines[0].Length;
+        int wordlen = word.Length;
+        int[] xdirection = { 0, 0, 1, -1, 1, 1, -1, -1 }; // up.down.right.left.botright.topright.downleft.topleft.
+        int[] ydirection = { 1, -1, 0, 0, 1, -1, 1, -1 };
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    int newx = i;
+                    int newy = j;
+                    int b;
+                    for (b = 0; b < wordlen; b++)
+                    {
+                        if (newx < 0 || newx >= length || newy < 0 || newy >= width || lines[newx][newy] != word[b])
+                            break;
+                        newx = newx + xdirection[k];
+                        newy = newy + ydirection[k];
+                    }
+                    if (b == wordlen)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
